f2fd7c0c8bf8f3f78c96c3233939be1d517c4e78
lijialiang <lijialiang@banggood.com>
2018-12-31 18:02:53 +0800

    获取手机分辨率

--- untils/phoneinfo.py
+++ untils/phoneinfo.py
@@ -31,5 +31,39 @@ def get_phone_info(devices):
     return l_list
 
 
+# 得到最大运行内存
+def get_men(devices):
+    cmd = "adb -s "+devices+ " shell cat /proc/meminfo"
+    get_cmd = os.popen(cmd).readlines()
+    men_total = 0
+    men_total_str = "MemTotal"
+    for line in get_cmd:
+        if line.find(men_total_str) >= 0:
+            men_total = line[len(men_total_str) +1:].replace("kB", "").strip()
+            break
+    return int(men_total)
+
+
+# 得到几核cpu
+def get_cpu(devices):
+    cmd = "adb -s " +devices +" shell cat /proc/cpuinfo"
+    get_cmd = os.popen(cmd).readlines()
+    find_str = "processor"
+    int_cpu = 0
+    for line in get_cmd:
+        if line.find(find_str) >= 0:
+            int_cpu += 1
+    return str(int_cpu) + "核"
+
+
+# 得到手机分辨率
+def get_pix(devices):
+    result = os.popen("adb -s " + devices+ " shell wm size", "r")
+    return result.readline().split("Physical size:")[1]
+
 if __name__ == '__main__':
     test = get_phone_info('192.168.42.101:5555')
+    test1 = get_men('192.168.42.101:5555')
+    test2 = get_cpu('192.168.42.101:5555')
+    test3 = get_pix('192.168.42.101:5555')
+    print(test3)
